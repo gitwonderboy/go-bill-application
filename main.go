c2c5016f6c447d7df3054513c968c48917c7c777
Wonderboy Mthiyane <codelad1@gmail.com>
2024-02-27 16:11:00 +0200

    Update: main.go Variables and Declairation

--- main.go
+++ main.go
@@ -2,8 +2,41 @@ package main
 
 import "fmt"
 
-func main() {
-	myName := "Wonderboy"
+// This way of declairation doesn't work outside the function
+// myName := "Wonderboy"
 
-	fmt.Println(myName)
+func main() {
+	// String Declairation
+	var name string = "Wonderboy"
+	myName := "Wonderboy"
+	var stringPlaceholder string
+
+	fmt.Println("My name is", name)
+	fmt.Println("My name is", myName)
+	fmt.Println("My name is", stringPlaceholder)
+	stringPlaceholder = "Wonderboy"
+	fmt.Println("My name is", stringPlaceholder)
+	fmt.Println("")
+
+	// Integer Declairation
+
+	var age int = 25
+	myAge := 25
+
+	var intPlaceholder int
+
+	fmt.Println("My age is ", age)
+	fmt.Println("My age is ", myAge)
+	fmt.Println("My age is ", intPlaceholder)
+	intPlaceholder = 25
+	fmt.Println("My age is ", intPlaceholder)
+	fmt.Println("")
+
+	// Arrays
+
+	ary := []any{"dog", "cat", "snake", 1, 50}
+
+	fmt.Println(ary)
+	fmt.Println(len(ary))
+	fmt.Println("")
 }
